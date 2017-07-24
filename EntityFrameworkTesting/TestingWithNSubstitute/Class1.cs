1d62baab810330105a3de1e077c10f703a57514a
panicoenlaxbox <panicoenlaxbox@gmail.com>
2017-07-25 00:59:01 +0200

    Links to NSubstitutes examples

---
+++ EntityFrameworkTesting/TestingWithNSubstitute/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkTesting.TestingWithNSubstitute
+{
+    /// <summary>
+    /// http://seekdavidlee.blogspot.com.es/2014/11/mocking-out-entity-framework.html
+    /// http://blog.learningtree.com/mock-entity-frameworks-dbcontext-unit-testing/
+    /// http://www.jerriepelser.com/blog/unit-testing-nbuilder-fake-or-mock-dbset/
+    /// http://thedatafarm.com/data-access/how-ef6-enables-mocking-dbsets-more-easily/
+    /// https://stackoverflow.com/questions/38333746/how-do-i-unit-test-a-repository-that-uses-dbcontext-with-nsubstitute
+    /// http://sinairv.github.io/blog/2015/10/04/mock-entity-framework-dbset-with-nsubstitute/
+    /// https://gist.github.com/jhoerr/9a064c83d04076fb056b98a30f4664f9
+    /// https://stackoverflow.com/questions/21069986/nsubstitute-dbset-iqueryablet
+    /// </summary>
+    class Class1
+    {
+    }
+}
